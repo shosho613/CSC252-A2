4170f94ef0e55e1635bd63a97caea33f05d3c6f3
Shoham Shitrit <shohami.sh@gmail.com>
2019-10-09 23:43:16 -0400

    updated a.c

--- a.c
+++ a.c
@@ -1,6 +1,23 @@
+ int NP = 14;
 
-long NP = 14.0;
-
-void ask(){
-	puts("Press CTRL+C to exit.");
-}
+ void ask(){
+     int d = 0;
+     while (d == 0) { 
+     puts("Enter Ctrl+C To Exit: ");
+     scanf("%d", d);
+     if(d != 0){
+         if( d > 4){
+             if(d < NP){
+                 printf("%d is just right!", d);
+                 return;
+             }else{
+                 puts("Whoops! That's too big. Please enter a smaller number!");
+             }
+         }else{
+             puts("Whoops! That's too small. Please enter a higher number!");
+         }
+     }
+     puts("Sorry, did not understand your input. Repeating question.");
+     }
+ 
+ }
