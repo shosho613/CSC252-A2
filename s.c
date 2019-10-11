a8bce134f457ac0522bb95c5948824cd4409a71d
Shoham Shitrit <shohami.sh@gmail.com>
2019-10-10 22:10:19 -0400

    Update s.c

--- s.c
+++ s.c
@@ -2,10 +2,25 @@
 
 int ncmp = 4;
 
-int x(uint64_t x, uint64_t y){
-	return 0;
+#include <stdint.h>
+uint64_t ncmp = 4;
+
+int x(uint64_t rdi, uint64_t rsi){
+    uint64_t rax = rdi;
+    uint32_t edx = (uint32_t) rax; // take highest 32 bits of both parameters
+    rax = rsi;
+    uint32_t edx2 = (uint32_t) rax;
+    rdi = rax;
+    return edx2;
+
 }
 
-void s(uint64_t x, uint64_t y){
+void s(uint64_t rdi, uint32_t esi){
+    int x= 1;
+
+    if(rdi > x){
+        
+    }
+
 
 }
