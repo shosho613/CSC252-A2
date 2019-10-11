13ec1605be33560323967bf6b4e0fa1d8ed31418
Shoham Shitrit <shohami.sh@gmail.com>
2019-10-11 01:05:29 -0400

    Update g.c

--- g.c
+++ g.c
@@ -1,4 +1,23 @@
 
-void gp(int x, int y){
-	
+void gp(uint64_t x,uint32_t y){
+    int rax = x;
+    int a = 0;
+    int b = 1;
+    int c = 3;
+    if (1 > x){
+       return;
+    }else{
+        x *= x;
+        if( 3 < x){
+            // goto .L9
+        }else{
+            return;
+        }
+    }
+
+
+
+
+
+
 }
